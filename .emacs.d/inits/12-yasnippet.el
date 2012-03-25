308aeadd9bb46272a9cc4bb7082f1733750c6603
okitan <okitakunio@gmail.com>
2012-03-25 11:54:58 +0900

    use yasnippet

---
+++ .emacs.d/inits/12-yasnippet.el
@@ -0,0 +1,10 @@
+(el-get 'sync 'yasnippet)
+
+(when (require 'yasnippet nil t)
+  (setq yas/root-directory
+	'("~/.emacs.d/etc/snippets"
+	  "~/.emacs.d/el-get/yasnippet/snippets"))
+  (mapc 'yas/load-directory yas/root-directory)
+
+  (yas/global-mode 1)
+)
