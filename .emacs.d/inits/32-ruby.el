5b551774cc0fc99baaae227a8f46991da8922254
okitan <okitakunio@gmail.com>
2012-04-22 09:02:31 +0900

    add setting for ruby-mode

---
+++ .emacs.d/inits/32-ruby.el
@@ -0,0 +1,4 @@
+(add-to-list 'auto-mode-alist '("\\Gemfile$"      . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Gemfile.lock$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Rakefile$"     . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Procfile$"     . ruby-mode))
