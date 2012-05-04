209607684439ac63a5361f902b79afba6180084b
okitan <okitakunio@gmail.com>
2012-05-04 12:11:17 +0900

    init-loader is installed by el-get

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,6 @@
 ;;; disable backup at first
 (setq backup-inhibited t)
 
-;;; load-path
-(add-to-list 'load-path "~/.emacs.d/elisp")
-
 ;;; el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 ;; if there is no el-get, install
@@ -14,15 +11,19 @@
      (let (el-get-master-branch)
        (end-of-buffer)
        (eval-print-last-sexp)))))
-;;  (with-current-buffer
-;;    (url-retrieve-synchronously
-;;       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-;;    (end-of-buffer)
-;;    (eval-print-last-sexp)))
-;; synchronous install
+
+;; original rpc
+(setq el-get-sources
+      '(
+        (:name init-loader
+               :type http
+               :description "init-loader"
+               :url "https://raw.github.com/gist/1021706/init-loader.el")
+        ))
 (el-get 'sync)
 
-;;; init-loader
-;; init-loader is installed by executing init.sh
-(when (require 'init-loader nil t)
-  (init-loader-load))
+;;; init-loade
+(el-get 'sync 'init-loader)
+
+(require 'init-loader)
+(init-loader-load)
