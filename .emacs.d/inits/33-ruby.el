51e0424d2fb2f6682c238df4d012421348a1a37c
okitan <okitakunio@gmail.com>
2012-05-13 09:32:19 +0900

    gemspec

--- .emacs.d/inits/33-ruby.el
+++ .emacs.d/inits/33-ruby.el
@@ -2,6 +2,7 @@
 (add-to-list 'auto-mode-alist '("\\Gemfile.lock$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Rakefile$"     . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Procfile$"     . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$"     . ruby-mode))
 
 ;;; rspec-mode
 (el-get 'sync 'rspec-mode)
