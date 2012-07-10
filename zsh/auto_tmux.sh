f911338fac23e23d2db538680ee5014d56f9be5e
okitan <okitakunio@gmail.com>
2012-07-10 12:08:13 +0900

    indent in ruby and fix auto_tmux

--- zsh/auto_tmux.sh
+++ zsh/auto_tmux.sh
@@ -2,9 +2,12 @@ function auto_tmux() {
     not_attached_sessions=`tmux ls 2>/dev/null | grep -v attached`
     if [[ `print $not_attached_sessions | wc -l` -eq 0 ]]; then
         tmux
+        tmux splitw -h
     else
         tmux attach -t `print $not_attached_sessions | head -n 1  | cut -d ":" -f 1`
     fi
 }
 
-auto_tmux
+if [[ "$TMUX" = "" ]];
+    auto_tmux
+fi
