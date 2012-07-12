8081df06783e9349402ac6980f8f20226d75e67c
okitan <okitakunio@gmail.com>
2012-07-12 12:24:20 +0900

    forgot then

--- zsh/auto_tmux.sh
+++ zsh/auto_tmux.sh
@@ -2,12 +2,11 @@ function auto_tmux() {
     not_attached_sessions=`tmux ls 2>/dev/null | grep -v attached`
     if [[ `print $not_attached_sessions | wc -l` -eq 0 ]]; then
         tmux
-        tmux splitw -h
     else
         tmux attach -t `print $not_attached_sessions | head -n 1  | cut -d ":" -f 1`
     fi
 }
 
-if [[ "$TMUX" = "" ]];
+if [[ "$TMUX" = "" ]]; then
     auto_tmux
 fi
