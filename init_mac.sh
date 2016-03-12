b1cfb68d51e88c1384142ef7e6def0419f81daf8
okitan <okitakunio@gmail.com>
2016-03-12 14:52:46 +0900

    add init script

---
+++ init_mac.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+# mac settings (NSGlobalDomain can be replaced with -g)
+## general
+function set_plist() {
+    file=${HOME}/Library/Preferences/$1
+    key=$2
+    type=$3
+    value=$4
+
+    /usr/libexec/PlistBuddy -c "Add ${key} ${type} ${value}" ${file} \
+||  /usr/libexec/PlistBuddy -c "Set ${key}         ${value}" ${file}
+}
+
+set_plist com.apple.recentitems.plist :RecentApplications:MaxAmount integer 0
+set_plist com.apple.recentitems.plist :RecentDocuments:MaxAmount    integer 0
+set_plist com.apple.recentitems.plist :RecentServers:MaxAmount      integer 5
+
+## spotlight
+# TODO:
+
+## keyboard
+defaults write NSGlobalDomain KeyRepeat        -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 35
+
+## mouse
+defaults write com.apple.driver.AppleBluetoothMultitouch.mouse MouseButtonMode -string "TwoButton"
+
+## trackpad
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int  2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick           -bool false
+
+# application settings
+## menu bar
+defaults write com.apple.menuextra.clock DateFormat -string "M\U6708d\U65e5(EEE)  H:mm"
+
+## finder
+defaults write NSGlobalDomain   AppleShowAllExtensions -bool   true
+defaults write com.apple.finder ShowTabView            -bool   true
+defaults write com.apple.finder NewWindowTarget        -string "PfLo"
+defaults write com.apple.finder NewWindowTargetPath    -string "file://${HOME}/Downloads/"
+# TODO: side bar
+
+## dock
+defaults write com.apple.dock autohide    -bool   true
+defaults write com.apple.dock orientation -string "left"
+
+# reload
+killall Dock
+killall SystemUIServer
+
+# more instructions
+echo install XCode from AppStore manually
+echo run: \`curl https://raw.githubusercontent.com/okitan/dotfiles/master/bootstrap.sh \| sh\`
