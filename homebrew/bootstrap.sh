f5ef591e537a6a3abfb9c6db327d82525af1e85e
okitan <okitakunio@gmail.com>
2016-03-12 17:54:22 +0900

    tap cask

--- homebrew/bootstrap.sh
+++ homebrew/bootstrap.sh
@@ -4,5 +4,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # plugins
-brew install caskroom/cask/brew-cask
-brew tap Homebrew/bundle
+brew tap caskroom/cask
