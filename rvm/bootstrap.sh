edfb50555a2e82f8943079b8ddbab68694552027
okitan <okitakunio@gmail.com>
2018-12-17 22:49:04 +0900

    :bulb: set it in rvm

--- rvm/bootstrap.sh
+++ rvm/bootstrap.sh
@@ -1,9 +1,18 @@
 #/bin/bash
 
-set -eux
+set -eu
 
 # install rvm
-if ! type rvm; then
-  gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB && \
-    curl -sSL https://get.rvm.io | bash -s stable
-fi
+( set -x
+  if ! type rvm; then
+    gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB && \
+      curl -sSL https://get.rvm.io | bash -s stable
+  fi
+)
+
+# link rvmrc
+(cd $(dirname $0) || (echo "cd failed" && exit 127)
+  set -x
+  [[ -d ~/.rvmrc && (! -L ~/.rvmrc ) ]] && mv ~/.rvmrc{,.bak}
+  ln -sf $(pwd)/.rvmrc ~
+)
