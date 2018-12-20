7db2306bdd65d830656940ed8f2ffe262e704879
okitan <okitakunio@gmail.com>
2018-12-20 11:42:47 +0900

    :bulb: add gke

--- osx/homebrew/k8s.sh
+++ osx/homebrew/k8s.sh
@@ -11,3 +11,11 @@ brew cask install docker --appdir=/Applications
 # k8s
 brew_install_or_upgrade kubernetes-cli
 brew_install_or_upgrade skaffold
+
+# gke
+brew cask install google-cloud-sdk
+
+# TODO: confirm works
+( set -e
+  [[ -s ~/.config/gcloud/access_tokens.db ]] || gcloud auth login
+)
