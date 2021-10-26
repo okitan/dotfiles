1528644c7d114570903efe333ea1409dd1874dfb
okitan <okitakunio@gmail.com>
2021-10-26 22:28:07 +0900

    :hospital:

--- homebrew/git.sh
+++ homebrew/git.sh
@@ -13,5 +13,5 @@ packages=(git git-delta tig hub)
   brew install "${packages[@]}"
 
   # I think export PATH="$PATH:$(brew --prefix git)/share/git-core/contrib/diff-highlight" seems also good
-  ln -sf "$(brew --prefix git)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/local/bin/diff-highlight
+  ln -sf "$(brew --prefix git)/share/git-core/contrib/diff-highlight/diff-highlight" "$(brew --prefix)/bin/diff-highlight"
 )
