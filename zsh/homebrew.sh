ab028de6a09f3ae8113207d8bc04219c2d185927
okitan <okitakunio@gmail.com>
2021-10-27 00:00:24 +0900

    :wrench:

--- zsh/homebrew.sh
+++ zsh/homebrew.sh
@@ -1,13 +1,11 @@
 load_homebrew() {
   if [[ -x /usr/local/bin/brew && $(echo "$PATH" | grep /usr/local/bin) ]]; then
     export PATH="/usr/local/bin:$PATH"
-    return 0
   else
     if [[ -x /opt/homebrew/bin/brew && $(echo "$PATH" | grep /opt/homebrew/bin) ]]; then
       export PATH="/opt/homebrew/bin:$PATH"
-      return 0
     else
-      return 1
+      echo "no homebrew installed"
     fi
   fi
 }
