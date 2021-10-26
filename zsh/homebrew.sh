c739b3586c05751760c51bdd6d431585594146a7
okitan <okitakunio@gmail.com>
2021-10-26 23:52:29 +0900

    :hospital:

--- zsh/homebrew.sh
+++ zsh/homebrew.sh
@@ -1,13 +1,13 @@
 load_homebrew() {
   if [[ -x /usr/local/bin/brew && $(echo "$PATH" | grep /usr/local/bin) ]]; then
     export PATH="/usr/local/bin:$PATH"
-    return true
+    return 0
   else
     if [[ -x /opt/homebrew/bin/brew && $(echo "$PATH" | grep /opt/homebrew/bin) ]]; then
       export PATH="/opt/homebrew/bin:$PATH"
-      return true
+      return 0
     else
-      return false
+      return 1
     fi
   fi
 }
