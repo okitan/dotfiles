a47c9f264cf6d01f5886867bfe18f35a16b77dda
okitan <okitakunio@gmail.com>
2021-10-27 00:02:49 +0900

    :ambulance: forgot not

--- zsh/homebrew.sh
+++ zsh/homebrew.sh
@@ -1,8 +1,8 @@
 load_homebrew() {
-  if [[ -x /usr/local/bin/brew && $(echo "$PATH" | grep /usr/local/bin) ]]; then
+  if [[ -x /usr/local/bin/brew && ! $(echo "$PATH" | grep /usr/local/bin) ]]; then
     export PATH="/usr/local/bin:$PATH"
   else
-    if [[ -x /opt/homebrew/bin/brew && $(echo "$PATH" | grep /opt/homebrew/bin) ]]; then
+    if [[ -x /opt/homebrew/bin/brew && ! $(echo "$PATH" | grep /opt/homebrew/bin) ]]; then
       export PATH="/opt/homebrew/bin:$PATH"
     else
       echo "no homebrew installed"
