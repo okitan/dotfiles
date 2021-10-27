8d1b7b7a51ab554a48dc09901b41cedf7e2daedb
okitan <okitakunio@gmail.com>
2021-10-27 13:24:45 +0900

    :hospital:

--- zsh/homebrew.sh
+++ zsh/homebrew.sh
@@ -1,9 +1,13 @@
 load_homebrew() {
-  if [[ -x /usr/local/bin/brew ]] && echo "$PATH" | grep -q /usr/local/bin; then
-    eval "$(/usr/local/bin/brew shellenv)"
+  if [[ -x /usr/local/bin/brew ]]; then
+    if ! echo "$PATH" | grep -q /usr/local/bina; then
+      eval "$(/usr/local/bin/brew shellenv)"
+    fi
   else
-    if [[ -x /opt/homebrew/bin/brew ]] && echo "$PATH" | grep -q /opt/homebrew/bin; then
-      eval "$(/opt/homebrew/bin/brew shellenv)"
+    if [[ -x /opt/homebrew/bin/brew ]]; then
+      if ! echo "$PATH" | grep -q /opt/homebrew/bin; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+      fi
     else
       echo "no homebrew installed"
     fi
